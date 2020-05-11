42a3f0a381070a499bbf63aa98043fc3b80b0a4f
Paul Debevec <paulmdebevec@gmail.com>
2020-05-11 17:45:14 -0600

    Uncomment before_action in admin base_controller

--- app/controllers/admin/api/v1/base_controller.rb
+++ app/controllers/admin/api/v1/base_controller.rb
@@ -1,5 +1,5 @@
 class Admin::Api::V1::BaseController < ActionController::API
-  # before_action :require_admin!
+  before_action :require_admin!
 
   def require_admin!
     four_oh_four unless current_user.admin?
