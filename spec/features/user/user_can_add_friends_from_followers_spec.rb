df9a4f93697b5fb3353570eae0528ca9704ed91b
Fred Rondina <fredrondina96@gmail.com>
2020-05-11 14:42:25 -0600

    added partial test for new friendships

---
+++ spec/features/user/user_can_add_friends_from_followers_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'User' do
+  it 'user can sign in' do
+    user_2 = User.create(
+     email: 'fredrondina96@gmail.com',
+     first_name: 'Fred',
+     last_name: 'Rondina',
+     password: 'password_1',
+     token: ENV["GITHUB_TOKEN_2"],
+     role: :default,
+   )
+   user_1 = User.create(
+     email: 'paulmdebevec@gmail.com',
+     first_name: 'Paul',
+     last_name: 'Debevec',
+     password: 'password_2',
+     token: ENV["GITHUB_TOKEN_1"],
+     role: :default,
+   )
+
+    user3 = create(:user)
+    user3.token = "No Key"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit(dashboard_path)
+
+    expect(page).to have_link("Add as Friend")
+
+  end
+end
