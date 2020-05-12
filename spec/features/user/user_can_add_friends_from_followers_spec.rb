218bed82509ecbca7571277a13b4ed49b960d242
Fred Rondina <fredrondina96@gmail.com>
2020-05-12 13:28:03 -0600

    added sad path testing for friends

--- spec/features/user/user_can_add_friends_from_followers_spec.rb
+++ spec/features/user/user_can_add_friends_from_followers_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 describe 'User' do
-  it 'user can sign in' do
+  it 'user can add friends from their dashboard' do
+    user_2 = User.create(
+     email: 'fredrondina96@gmail.com',
+     first_name: 'Fred',
+     last_name: 'Rondina',
+     password: 'password_1',
+     login: 'fredrondina96',
+     token: ENV["GITHUB_TOKEN_2"],
+     role: :default,
+   )
+   user_1 = User.create(
+     email: 'paulmdebevec@gmail.com',
+     first_name: 'Paul',
+     last_name: 'Debevec',
+     password: 'password_2',
+     login: 'PaulDebevec',
+     token: ENV["GITHUB_TOKEN_1"],
+     role: :default,
+   )
+
+    user3 = create(:user)
+    user3.token = "No Key"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+    visit(dashboard_path)
+
+    within '.followers' do
+      expect(page).to have_link("Add as Friend")
+      click_link("Add as Friend")
+    end
+
+    expect(page).to have_content("Successfully Added Friend!")
+    expect(user_1.friends.length).to eq(1)
+    expect(current_path).to eq(dashboard_path)
+  end
+  it "cannot add a friend that you are already friends with" do
     user_2 = User.create(
      email: 'fredrondina96@gmail.com',
      first_name: 'Fred',
@@ -28,10 +64,13 @@ describe 'User' do
 
     visit(dashboard_path)
     expect(page).to have_link("Add as Friend")
-    click_link("Add as Friend")
 
-    expect(page).to have_content("Successfully Added Friend!")
-    expect(user_1.friends.length).to eq(1)
-    expect(current_path).to eq(dashboard_path)
+    within '.following' do
+      expect(page).to have_link("Add as Friend")
+      click_link("Add as Friend")
+    end
+
+    visit(dashboard_path)
+    expect(page).to_not have_link("Add as Friend")
   end
 end
