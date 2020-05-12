3182e5538795e44dec953653fc3adea9d80c9e50
Fred Rondina <fredrondina96@gmail.com>
2020-05-12 12:59:56 -0600

    added flash message and redirect for adding new friends

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,9 @@
 class FriendsController < ApplicationController
   def create
     current_user.add_friend(params["login"])
+    flash[:success] = "Successfully Added Friend!"
+    current_user.reload
+
+    redirect_to dashboard_path
   end
 end
